a02ac80ba4dde039845f7c745edf512c7bcd34c3
Alexbardjo <alexbardjo@gmail.com>
2020-04-08 22:01:50 +0300

    Second commit props in App lesson 27

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import s from './Dialogs.module.css';
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
+
+
+const Dialogs = (props) => {
+
+    let {dialogs, messages} = props;
+
+    let dialogsElements = dialogs
+        .map(d => <DialogItem name={d.name} id={d.id}/>);
+    let messagesElements = messages
+        .map(m => <Message message={m.message}/>);
+
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                {dialogsElements}
+            </div>
+            <div className={s.messages}>
+                {messagesElements}
+            </div>
+        </div>
+    )
+};
+
+export default Dialogs;
