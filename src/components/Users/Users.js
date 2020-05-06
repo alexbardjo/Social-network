0943c193450dc969d83fd2bad610091544d83130
Alexbardjo <alexbardjo@gmail.com>
2020-05-06 12:10:56 +0300

    lesson 51 Users API

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,27 +1,23 @@
 import React from "react";
 import styles from './users.module.css';
-
+import * as axios from "axios";
+import UserPhoto from '../../assets/images/User.png';
 
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers (
-            [ {id: 1,
-                photoURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQmEQzTqrZwy_tN2QsC-XkOwh7wuVlolpDMoxR5qBwJBzQrPiGg&usqp=CAU',
-                followed: false, fullName: 'Alexandr', status: 'I am a star',
-                location: {city:'Krivoy Rog', country: 'Ukraine'}},
-                {id: 2, photoURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR8ByYzeUFXM_E_GFi4XU3m5hGJAZTrLpgu6WWquIZSLsddsCvf&usqp=CAU', followed: true, fullName: 'Vladislav', status: 'I am a boss', location: {city:'Kiev', country: 'Ukraine'}},
-                {id: 3, photoURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQmEQzTqrZwy_tN2QsC-XkOwh7wuVlolpDMoxR5qBwJBzQrPiGg&usqp=CAU', followed: true, fullName: 'Ivan', status: 'I am a rock', location: {city:'Moscow', country: 'Russia'}},
-                {id: 4, photoURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR8ByYzeUFXM_E_GFi4XU3m5hGJAZTrLpgu6WWquIZSLsddsCvf&usqp=CAU', followed: false, fullName: 'Dmitriy', status: 'I am a rock too', location: {city:'Minsk', country: 'Balarus'}},
-            ]
-        )
-    }
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items);
+
+        })
+    };
 
     return <div>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoURL} className={styles.usersPhoto}/>
+                        <img src={u.photos.small != null ? u.photos.small : UserPhoto} className={styles.usersPhoto}/>
                     </div>
                     <div>
                         {u.followed ?
@@ -36,12 +32,12 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{'u.location.country'}</div>
+                        <div>{'u.location.city'}</div>
                     </span>
                 </span>
             </div>)
